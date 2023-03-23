3d975cf071da49e20c58656e5f84bc16667b0abd
Matan_Buljubasic <mbuljubasic@etfos.hr>
2023-03-23 13:33:52 +0100

    Finish zad2.py

--- LV4/zad2.py
+++ LV4/zad2.py
@@ -9,10 +9,20 @@ data = pd.read_csv('data_C02_emission.csv')
 ohe = OneHotEncoder()
 encoded = pd.DataFrame(ohe.fit_transform(data[['Fuel Type']]).toarray())
 data = data.join(encoded)
-data.rename('')
-
+data = data.rename(columns={0:'D', 1:'E', 2:'X', 3:'Z'})
 y = data['CO2 Emissions (g/km)'].copy()
-X = data.drop('CO2 Emissions (g/km)', axis=1)
+X = data[['Engine Size (L)', 'Cylinders', 'Fuel Consumption City (L/100km)', 'Fuel Consumption Hwy (L/100km)', 'Fuel Consumption Comb (L/100km)', 'Fuel Consumption Comb (mpg)', 'D', 'E', 'X', 'Z']]
 
 X_train , X_test , y_train , y_test = train_test_split (X, y, test_size = 0.2)
+
+linearModel = lm.LinearRegression()
+linearModel.fit(X_train, y_train)
+
+y_test_p = linearModel.predict(X_test)
+
+
+data['Error'] = abs(y_test-y_test_p)
+
+print(data[data['Error'] == data['Error'].max()])
+
 
